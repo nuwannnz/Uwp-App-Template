1c0d7c1ee7ade84405148678abf782fc38db8b88
Nuwan Karunarathna <nuwankarunarathna504@gmail.com>
2016-12-06 16:41:58 +0530

    Add files via upload

---
+++ LeepNext.MyWeek.Test/UnitTest.cs
@@ -0,0 +1,15 @@
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeepNext.MyWeek.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
